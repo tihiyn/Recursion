8ce03f980443f1de060bc58b51b55e114b45d461
Nikita Tikhomirov <tihomnik@yandex.ru>
2025-01-16 13:05:46 +0300

    Made the function polymorphic

--- Task7.java
+++ Task7.java
@@ -1,20 +1,28 @@
 import java.util.List;
 
-public class Task7 {
-    public int findSecondMax(List<Integer> list) {
-        return findSecondMax(list, 0, Integer.MIN_VALUE, Integer.MIN_VALUE);
+public class Task7<T extends Comparable<T>> {
+    public T findSecondMax(List<T> list) {
+        if (list.isEmpty() || list.size() == 1) {
+            return null;
+        }
+
+        if (list.get(0).compareTo(list.get(1)) <= 0) {
+            return findSecondMax(list, 0, list.get(0), list.get(1));
+        }
+
+        return findSecondMax(list, 0, list.get(1), list.get(0));
     }
 
-    private int findSecondMax(List<Integer> list, final int index, int firstMax, int secondMax) {
+    private T findSecondMax(List<T> list, final int index, T firstMax, T secondMax) {
         if (index == list.size()) {
             return secondMax;
         }
 
-        if (list.get(index) > secondMax && list.get(index) < firstMax) {
+        if (list.get(index).compareTo(secondMax) > 0 && list.get(index).compareTo(firstMax) < 0) {
             secondMax = list.get(index);
         }
 
-        if (list.get(index) >= firstMax) {
+        if (list.get(index).compareTo(firstMax) >= 0) {
             secondMax = firstMax;
             firstMax = list.get(index);
         }
